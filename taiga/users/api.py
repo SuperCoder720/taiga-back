SuperCoder720/taiga-back

commit 27f12f7be90f6c797991570cf76d75f29dbc0d57

Fixed security bug on users info

taiga/users/api.py
@@ -54,8 +54,7 @@ class MembersFilterBackend(BaseFilterBackend):
                 return queryset.filter(Q(memberships__project=project) | Q(id=project.owner.id)).distinct()
             else:
                 raise exc.PermissionDenied(_("You don't have permisions to see this project users."))
-        else:
-            return queryset
+        return []
 
 
 class UsersViewSet(ModelCrudViewSet):

taiga/users/permissions.py
@@ -27,7 +27,7 @@ class IsTheSameUser(PermissionComponent):
 class UserPermission(TaigaResourcePermission):
     enought_perms = IsSuperUser()
     global_perms = None
-    retrieve_perms = AllowAny()
+    retrieve_perms = IsTheSameUser()
     update_perms = IsTheSameUser()
     destroy_perms = IsTheSameUser()
     list_perms = AllowAny()

commit 43f4c496d85526f30e00db70eb0b64f14fad7cc3

Fix small test fails introduced in the commit 27f12f7

taiga/users/api.py
@@ -54,6 +54,10 @@ class MembersFilterBackend(BaseFilterBackend):
                 return queryset.filter(Q(memberships__project=project) | Q(id=project.owner.id)).distinct()
             else:
                 raise exc.PermissionDenied(_("You don't have permisions to see this project users."))
+
+        if request.user.is_superuser:
+            return queryset
+
         return []
 
 
@@ -61,10 +65,20 @@ class UsersViewSet(ModelCrudViewSet):
     permission_classes = (permissions.UserPermission,)
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
-    filter_backends = (MembersFilterBackend,)
 
     def create(self, *args, **kwargs):
         raise exc.NotSupported()
+
+    def list(self, request, *args, **kwargs):
+        self.object_list = MembersFilterBackend().filter_queryset(request, self.get_queryset(), self)
+
+        page = self.paginate_queryset(self.object_list)
+        if page is not None:
+            serializer = self.get_pagination_serializer(page)
+        else:
+            serializer = self.get_serializer(self.object_list, many=True)
+
+        return Response(serializer.data)
 
     @list_route(methods=["POST"])
     def password_recovery(self, request, pk=None):

tests/integration/resources_permissions/test_users_resources.py
@@ -44,7 +44,7 @@ def test_user_retrieve(client, data):
     ]
 
     results = helper_test_http_method(client, 'get', url, None, users)
-    assert results == [200, 200, 200, 200]
+    assert results == [401, 200, 403, 200]
 
 
 def test_user_update(client, data):
@@ -82,21 +82,21 @@ def test_user_list(client, data):
 
     response = client.get(url)
     users_data = json.loads(response.content.decode('utf-8'))
-    assert len(users_data) == 3
+    assert len(users_data) == 0
     assert response.status_code == 200
 
     client.login(data.registered_user)
 
     response = client.get(url)
     users_data = json.loads(response.content.decode('utf-8'))
-    assert len(users_data) == 3
+    assert len(users_data) == 0
     assert response.status_code == 200
 
     client.login(data.other_user)
 
     response = client.get(url)
     users_data = json.loads(response.content.decode('utf-8'))
-    assert len(users_data) == 3
+    assert len(users_data) == 0
     assert response.status_code == 200
 
     client.login(data.superuser)